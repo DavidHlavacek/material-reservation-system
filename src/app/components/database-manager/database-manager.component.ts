6287bfd5729556a9253c09c4f156c5d725b34cac
David Hlavacek <davi.hlav@gmail.com>
2023-11-16 19:42:50 +0100

    Project Structure Done

---
+++ src/app/components/database-manager/database-manager.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-database-manager',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './database-manager.component.html',
+  styleUrl: './database-manager.component.css'
+})
+export class DatabaseManagerComponent {
+
+}
---
+++ src/app/components/entry-information/entry-information.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-entry-information',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './entry-information.component.html',
+  styleUrl: './entry-information.component.css'
+})
+export class EntryInformationComponent {
+
+}
