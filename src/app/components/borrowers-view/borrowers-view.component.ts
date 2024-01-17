60e2f3d451727fd8f050134f4b5c871365991716
DavidHlavacek <davi.hlav@gmail.com>
2024-01-17 14:45:21 +0100

    Nefeli's sidebar fix

--- src/app/components/borrowers-view/borrowers-view.component.ts
+++ src/app/components/borrowers-view/borrowers-view.component.ts
@@ -1,13 +1,68 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ItemService } from '../../services/item.service';
+import { Item } from '../../models/Item';
+
 
 @Component({
   selector: 'app-borrowers-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    MatTableModule,
+    MatCheckboxModule, 
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDividerModule],
   templateUrl: './borrowers-view.component.html',
   styleUrl: './borrowers-view.component.css'
 })
 export class BorrowersViewComponent {
+//   columnsToDisplay: string[] = [
+//     'select',
+//     'UserID',
+//     'Name',
+//     'Email',
+//     'NFCId'
+//   ];
 
+//   initialSelection = [];
+//   allowMultiSelect = true;
+//   selection = new SelectionModel<Item>(
+//     this.allowMultiSelect,
+//     this.initialSelection,
+//   );
+
+//   dataSource = new MatTableDataSource<Item>();
+
+//   isAllSelected() {
+//     const numSelected = this.selection.selected.length;
+//     const numRows = this.dataSource.data.length;
+//     return numSelected == numRows;
+//   }
+
+//   toggleAllRows() {
+//     this.isAllSelected()
+//       ? this.selection.clear()
+//       : this.dataSource.data.forEach((row) => this.selection.select(row));
+//   }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,12 +2,16 @@ import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: 'sidebar.component.html',
   styleUrls: ['sidebar.component.css'],
-  imports: [CommonModule, MatSidenavModule]
+  standalone: true,
+  imports: [CommonModule, MatSidenavModule, MatListModule]
 })
 export class SidebarComponent {
   @ViewChild(MatSidenav) sidenav!: MatSidenav;
